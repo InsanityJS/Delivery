dd55217de4d73d47cdd3eaa325cf577f45618191
Naumenko <maks.naymenko@gmail.com>
2021-01-17 19:24:31 +0200

    Maked slider and styled section Question

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".slider").slick({
+        dots: false,
+        infinite: true,
+        arrows: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+    });
+});
